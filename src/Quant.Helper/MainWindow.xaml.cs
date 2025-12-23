e8ba1cd9b4c644cdd36feaf27237391e963405b9
Vladyslav Hnatovskyi <vhnatovskyi@hotmail.com>
2025-12-23 10:22:58 -0700

    feat(Main): Add Transparancy Click

--- src/Quant.Helper/MainWindow.xaml.cs
+++ src/Quant.Helper/MainWindow.xaml.cs
@@ -2,9 +2,11 @@
 using Quant.Helper.Scripts;
 using Quant.Helper.Scripts.Abstractions;
 using SharpHook;
+using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Markup;
 using System.Windows.Threading;
 using WindowsInput;
@@ -19,26 +21,51 @@ public partial class MainWindow : Window, IComponentConnector
     private readonly Task _dispatcherTask;
     private readonly Dispatcher _dispatcher;
     private readonly ILogger _logger;
+    private readonly IGlobalHook _hook;
+    private readonly InputSimulator _inputSimulator;
+    private bool _isClickThrough = false;
+    private bool _ctrlPressed = false;
+
+    private const int GWL_EXSTYLE = -20;
+    private const int WS_EX_LAYERED = 0x80000;
+    private const int WS_EX_TRANSPARENT = 0x20;
 
     public MainWindow()
     {
         InitializeComponent();
         _logger = new Logger();
 
-        var globalHook = new SimpleGlobalHook();
-        var inputSimulator = new InputSimulator();
+        _hook = new SimpleGlobalHook();
+        _inputSimulator = new InputSimulator();
         var scriptList = new List<IScript>
         {
-            new TreeChopScript(inputSimulator),
-            new ElectricScript(_logger, inputSimulator),
-            new MineScript(_logger, inputSimulator)
+            new TreeChopScript(_inputSimulator),
+            new ElectricScript(_logger, _inputSimulator),
+            new MineScript(_logger, _inputSimulator)
         };
         _scripts = scriptList;
         ScriptList.ItemsSource = _scripts;
-        _dispatcher = new Dispatcher(_logger, globalHook, _scripts);
+        _dispatcher = new Dispatcher(_logger, _hook, _scripts);
         _dispatcher.ActiveScriptChanged += OnActiveScriptChanged;
         _dispatcher.OnScriptStopped += OnScriptStopped;
         _dispatcher.OnScriptRunning += OnScriptRunning;
+
+        _hook.KeyPressed += (_, e) =>
+        {
+            if (e.Data.KeyCode == SharpHook.Data.KeyCode.VcLeftControl || e.Data.KeyCode == SharpHook.Data.KeyCode.VcRightControl)
+                _ctrlPressed = true;
+
+            if (_ctrlPressed && e.Data.KeyCode == SharpHook.Data.KeyCode.VcH)
+            {
+                Dispatcher.BeginInvoke(new Action(ToggleTransparency));
+            }
+        };
+
+        _hook.KeyReleased += (_, e) =>
+        {
+            if (e.Data.KeyCode == SharpHook.Data.KeyCode.VcLeftControl || e.Data.KeyCode == SharpHook.Data.KeyCode.VcRightControl)
+                _ctrlPressed = false;
+        };
 
         LogList.ItemsSource = _logger.Messages;
         _logger.Messages.CollectionChanged += (_, __) =>
@@ -103,6 +130,31 @@ public partial class MainWindow : Window, IComponentConnector
         ActionPanel.Visibility = script != null ? Visibility.Visible : Visibility.Collapsed;
     }
 
+    private void ToggleTransparency()
+    {
+        _isClickThrough = !_isClickThrough;
+
+        var hwnd = new WindowInteropHelper(this).Handle;
+
+        if (_isClickThrough)
+        {
+            Opacity = 0.0;
+
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_LAYERED | WS_EX_TRANSPARENT);
+
+            _logger.Log("Вікно стало прозорим і click-through (Ctrl+H для відновлення)");
+        }
+        else
+        {
+            Opacity = 1;
+
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle & ~WS_EX_TRANSPARENT);
+            _logger.Log("Прозорість і click-through скасовано");
+        }
+    }
+
     private async void Exit_Click(object sender, RoutedEventArgs e)
     {
         try
@@ -116,4 +168,10 @@ public partial class MainWindow : Window, IComponentConnector
             Environment.Exit(0);
         }
     }
+
+    [DllImport("user32.dll")]
+    private static extern int GetWindowLong(IntPtr hwnd, int index);
+
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
 }
